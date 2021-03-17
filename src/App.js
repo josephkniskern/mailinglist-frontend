936de17ea9c77cbef6ff7dff61a98ba4ac508f48
Joseph Kniskern <kniskernjoseph@yahoo.com>
2021-03-17 11:40:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./components/Login";
+import { Grid, Avatar } from "@material-ui/core";
+import { Wrapper } from "./App.styles";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        direction="column"
+        style={{ minHeight: "100vh" }}
+        spacing={5}
+      >
+        <Grid item>
+          <Avatar
+            alt="Cat"
+            src="https://media.istockphoto.com/photos/european-shorthair-wearing-a-suit-beige-background-picture-id489543113?k=6&m=489543113&s=612x612&w=0&h=EiakKxqTM9FwxOkPNn3Ep_C_WaVbX-H0Nu_nsbHGcC0="
+            className={classes.large}
+          />
+        </Grid>
+        <Grid item>
+          <Login />
+        </Grid>
+      </Grid>
+    </Wrapper>
   );
 }
-
-export default App;
---
+++ src/App.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  border: 3px solid gainsboro;
+  border-radius: 30px;
+  margin: 20px;
+`;
